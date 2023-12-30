ff144ac8929871205b78c5179b825c5bd051f9c3
veganum <veganum@hotmail.com>
2023-12-31 00:25:05 +0100

    reconfigurando eslint y prettier

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   darkMode: 'class',
-  content: [
-    "./src/**/*.{html,ts}",
-  ],
+  content: ['./src/**/*.{html,ts}'],
   theme: {
     extend: {},
   },
   plugins: [],
-}
-
+};
